fb413d51143fc520c2807758e2886cf83b464367
Helio Frota <00hf11@gmail.com>
2025-04-03 18:18:19 -0300

    update to rust 1.86 and OTEL updates

--- src/otel.rs
+++ src/otel.rs
@@ -1,26 +1,24 @@
+use opentelemetry::global;
 use opentelemetry::metrics::Counter;
-use opentelemetry::{global, KeyValue};
-use opentelemetry_otlp::MetricExporter;
-use opentelemetry_sdk::metrics::{PeriodicReader, SdkMeterProvider};
+// use opentelemetry_otlp::MetricExporter;
 use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::metrics::{PeriodicReader, SdkMeterProvider};
 
 pub fn init_metrics(name: &str) {
     #[allow(clippy::expect_used)]
-    let exporter = MetricExporter::builder()
-        .with_tonic()
-        .build()
-        .expect("Unable to setup metrics exporter.");
+    // let exporter = MetricExporter::builder()
+    //     .with_tonic()
+    //     .build()
+    //     .expect("Unable to setup metrics exporter.");
+    let exporter = opentelemetry_stdout::MetricExporter::default();
+    let reader = PeriodicReader::builder(exporter).build();
 
-    // let exporter = opentelemetry_stdout::MetricExporter::default();
-    let reader =
-        PeriodicReader::builder(exporter, opentelemetry_sdk::runtime::TokioCurrentThread).build();
-
+    let resource = Resource::builder()
+        .with_service_name(name.to_string())
+        .build();
     let meter_provider = SdkMeterProvider::builder()
         .with_reader(reader)
-        .with_resource(Resource::new(vec![KeyValue::new(
-            "service.name",
-            name.to_string(),
-        )]))
+        .with_resource(resource)
         .build();
     global::set_meter_provider(meter_provider.clone());
 }
